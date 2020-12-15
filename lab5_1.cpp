58f73dffa0a09150d9f1ff4a9d0ed97d1c2b8de4
pulom999 <pulom999@gmail.com>
2020-12-15 16:52:34 +0700

    Filled out README

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -2,8 +2,30 @@
 using namespace std;
 
 int main(){
+    int x;
+    int even=0;
+    int odd=0;
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> x;
+
+    if (x !=0){
+        while (x !=0)
+        {
+           if (x%2==0)
+           {
+               even=even+1;
+           }else
+           {
+               odd=odd+1;
+           }
+        cout << "Enter an integer: ";
+        cin >> x;
+           
+        }
+        
+
+    }
+    cout << "#Even numbers = "<<even<<"\n";
+    cout << "#Odd numbers = "<<odd;
     return 0;
 }
